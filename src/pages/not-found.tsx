4cab544cc861a6eab258be8f13336bfbe5f250f3
Rodrigo Paixao <rodrigopaixao@rodrigos-mbp.home>
2025-02-18 15:33:01 -0500

    added 404 page

---
+++ src/pages/not-found.tsx
@@ -0,0 +1,16 @@
+import { Link } from 'react-router'
+
+export function NotFound() {
+  return (
+    <div className="flex h-screen flex-col items-center justify-center gap-2">
+      <h1 className="text-4xl font-bold">Page not Found (404)</h1>
+
+      <p className="text-accent-foreground">
+        Return to{' '}
+        <Link to="/" className="text-sky-600 dark:text-sky-400">
+          Dashboard
+        </Link>
+      </p>
+    </div>
+  )
+}
--- src/routes.tsx
+++ src/routes.tsx
@@ -6,11 +6,13 @@ import { Dashboard } from '@/pages/app/dashboard/dashboard.tsx'
 import { Orders } from '@/pages/app/orders/orders.tsx'
 import { SignIn } from '@/pages/auth/sign-in.tsx'
 import { SignUp } from '@/pages/auth/sign-up.tsx'
+import { NotFound } from '@/pages/not-found.tsx'
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
+    errorElement: <NotFound />,
     children: [
       {
         path: '/',
