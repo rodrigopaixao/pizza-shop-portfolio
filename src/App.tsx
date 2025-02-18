62dcf5aef100a5d2dcf563b8ba6cad1de2b78cb4
Rodrigo Paixao <rodrigopaixao@rodrigos-mbp.home>
2025-02-17 22:41:28 -0500

    shadcn installed

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import './index.css'
+import { Button } from "@/components/ui/button.tsx";
+
 export function App() {
     return (
-        <>
-            <h1>Hello World</h1>
-        </>
+        <div className="flex justify-center items-center h-screen">
+            <Button>Test</Button>
+        </div>
     )
 }
