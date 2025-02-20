64d74e6da197ea003e7c216cfeeef58ff4a5c29e
Rodrigo Paixao <rodrigopaixao@gmail.com>
2025-02-20 18:18:50 -0500

    added auto logout feature

--- src/pages/_layouts/app-layout.tsx
+++ src/pages/_layouts/app-layout.tsx
@@ -1,8 +1,30 @@
-import { Outlet } from 'react-router'
+import { useEffect } from 'react'
+import { Outlet, useNavigate } from 'react-router'
 
 import { Header } from '@/components/header'
+import { api } from '@/lib/axios'
 
 export function AppLayout() {
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const interceptorId = api.interceptors.response.use(
+      (response) => response,
+      (error) => {
+        const statusCode = error.response?.status
+        const errorCode = error.response?.data.code
+
+        if (statusCode === 401 && errorCode === 'UNAUTHORIZED') {
+          navigate(`/sign-in`, { replace: true })
+        }
+      },
+    )
+
+    return () => {
+      api.interceptors.response.eject(interceptorId)
+    }
+  }, [navigate])
+
   return (
     <div className="flex flex-col antialiased">
       <Header />
